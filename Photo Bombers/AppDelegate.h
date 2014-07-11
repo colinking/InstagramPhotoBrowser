48816ceb39b0891179e60bfbd3bad858879b4436
Colin <colin.king.96@gmail.com>
2014-07-11 13:29:01 -0400

    Initial Commit

---
+++ Photo Bombers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photo Bombers
+//
+//  Created by Colin on 7/11/14.
+//  Copyright (c) 2014 Colin King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photo Bombers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photo Bombers
+//
+//  Created by Colin on 7/11/14.
+//  Copyright (c) 2014 Colin King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
